8130e62f7ec3782dba9cd1b5ce3cb4237d90bdb6
omegabased <60020640+uglyfuckspider@users.noreply.github.com>
2021-07-06 19:51:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Flip_a_coid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coid flipper downloaded");
+            Console.WriteLine("Please enter how many times flip a coin");
+            uint flipTimes = uint.Parse(Console.ReadLine());
+            uint flipTimesDone = 0;
+            uint eagleSideTimes = 0;
+            uint tailsSideTimes = 0;
+            Random x = new Random();
+            while (flipTimesDone <= flipTimes)
+            {
+                ++flipTimesDone;
+                int coinSide = x.Next(3);
+                if (coinSide == 1)
+                {
+                    ++eagleSideTimes;
+                }    
+                if(coinSide == 2)
+                {
+                    ++tailsSideTimes;
+                }  
+            }
+            Console.WriteLine("Eagle side dropt: " + eagleSideTimes + "times");
+            Console.WriteLine("Tails side dropt: " + tailsSideTimes + "times");
+            Console.ReadLine();
+        }
+
+    }
+}
